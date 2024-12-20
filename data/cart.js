4c71349a0b2fe3d6d7f433dc2df01fff68ebb0f6
Tejas <ttejas220@gmail.com>
2024-12-20 20:06:59 +0530

    generate html for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];
+export const cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+},
+{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 1
+}];
 
 export function addToCart(productId){
     let matchingItem;
