ed05243c14391a6f5cbfedc7d2db90faf823b997
Tejas <ttejas220@gmail.com>
2024-12-26 00:02:06 +0530

    learn error handeling

--- data/products.js
+++ data/products.js
@@ -103,6 +103,8 @@ export function loadProductsFetch() {
     });
   
     console.log('load products');
+  }).catch(() =>{
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -128,12 +130,15 @@ export function loadProducts(fun){
 
   fun();
  });
+
+ xhr.addEventListener('error', (error) =>{
+  console.log('Unexpected error. Please try again later.');
+ });
  
  xhr.open('GET', 'https://supersimplebackend.dev/products');
  xhr.send();
 
 }
-
 /*
 export const products = [
   {
