e8931123ce3b01145557af3d013a2481e3f69503
Tejas <ttejas220@gmail.com>
2024-12-25 13:23:11 +0530

    use inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -33,7 +33,48 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
+
+class Clothing extends Product {
+sizeChartLink;
+
+// If you don't create a constructor then by default it uses the parent class constructor.
+constructor(productDetails){
+  //This will call the constructor of the parent class
+  super(productDetails);
+  this.sizeChartLink = productDetails.sizeChartLink;
+}
+//This method will Override/replace the parent's method
+extraInfoHTML(){
+  return `
+   <a href="${this.sizeChartLink}" target ="_blank">Size chart</a>
+  `;
+}
+
+}
+const tshirt = new Clothing({
+  id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+  name: "Adults Plain Cotton T-Shirt - 2 Pack",
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  priceCents: 799,
+  keywords: [
+    "tshirts",
+    "apparel",
+    "mens"
+  ],
+  type: "clothing",
+  sizeChartLink: "images/clothing-size-chart.png"
+});
+console.log(tshirt);
+console.log(tshirt.getPrice());
 
 export const products = [
   {
@@ -695,5 +736,8 @@ export const products = [
     ]
   }
 ].map((productDetails) =>{
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
