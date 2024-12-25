ee7f92b3d4a1e569c905079d1a6bf4a3bca798d1
Tejas <ttejas220@gmail.com>
2024-12-25 23:43:06 +0530

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,19 @@ import { loadCart } from '../data/cart.js';
 //import'../data/cart-class.js';
 //import '../data/backend-practice.js';
 
+async function loadPage(){
+    await loadProductsFetch();
+    await new Promise((resolve) =>{
+        loadCart(() => {
+            resolve();
+        });
+    });
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
     loadProductsFetch(),
     new Promise((resolve) =>{
@@ -17,6 +30,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve)=>{
