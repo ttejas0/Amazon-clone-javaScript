ea4b02bb90977b553a1da7a5f5c5d862f1e450fb
Tejas <ttejas220@gmail.com>
2024-12-25 23:04:54 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,57 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentsSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 //import'../data/cart-class.js';
 //import '../data/backend-practice.js';
-loadProducts(() =>{
+
+Promise.all([
+    new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve('value1'); 
+        });
+    
+    }),
+    new Promise((resolve) =>{
+        loadCart(() => {
+            resolve();
+        });
+    })
+
+]).then(() =>{
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('value1'); 
+    });
+
+}).then((value) =>{
+
+    console.log(value);
+    return new Promise((resolve) =>{
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+}).then(() =>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+// The reason why we use promise is when we use multiple callbacks it causes lot of nesting. lot of nesting means lots of indents. promises solve this problems by flatening the code.
+
+/*
+loadProducts(() =>{
+    loadCart(()=>{
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/
 
